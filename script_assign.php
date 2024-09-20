ec7658f2551983b47d35d90fd5651df09cd6d527
atuheivan <atuheivon@gmail.com>
2024-09-20 13:45:39 +0300

    Create script_assign.php

---
+++ script_assign.php
@@ -0,0 +1,15 @@
+<?php
+$nam="Atuhe Ivan.";
+$dat="7/08/2002.";
+$address="Mbarara.";
+echo"Nam:".$nam;
+echo"\nDOB:".$date;
+echo"\nAddress:".$address;
+echo"\n You are welcome home ".$name;
+$current=2024;
+$previous=2002;
+$Age=$current -$previous;
+echo " and you are now ".$Age."years old.";
+
+
+?>
